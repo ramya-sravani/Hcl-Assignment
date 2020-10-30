2c467dea68b57542bd9816fec669b353f25816ac
ramya-sravani <69158847+ramya-sravani@users.noreply.github.com>
2020-10-31 00:27:15 +0530

    Add files via upload

---
+++ Assignment5/ThreadConcurrently.java
@@ -0,0 +1,30 @@
+package Assignment5;
+
+
+class MultiThreaded implements Runnable {
+
+	public void run() {
+		
+		for(int i=0;i<=100;i++) {
+			System.out.println("Current thread name: " + Thread.currentThread().getName() + " " + i);
+		}
+	}
+	
+}
+public class ThreadConcurrently {
+	
+	public static void main(String[] args) {
+		
+		MultiThreaded multiThread = new MultiThreaded();
+		Thread thread1 = new Thread(multiThread, "Thread1");
+		Thread thread2 = new Thread(multiThread, "Thread2");
+		Thread thread3 = new Thread(multiThread, "Thread3");
+		
+		thread1.start();
+		thread2.start();
+		thread3.start();
+		
+	}
+
+}
+
